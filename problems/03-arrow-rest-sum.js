7a270bd9b54df5f3bcb5195a94e0be9303369ccd
Amanda Burton <amanda.kr2.3@gmail.com>
2022-12-02 16:59:36 -0500

    next solve

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,7 +10,14 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here
+const arrowRestSum = (...sum) => {
+  let num = 0
+  sum.forEach((element) => {
+    num += element
+  })
+return num
+}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
