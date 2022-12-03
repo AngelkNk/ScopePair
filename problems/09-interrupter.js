45a945abf3e9c78d72c46fc45d123c2d6cfc86ab
Jonathan  Rodriguez <jurodriguez949@gmail.com>
2022-12-02 16:08:05 -0800

    ten

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -14,9 +14,22 @@ let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 
-***********************************************************************/
+*********************************************************************/
 
-// Your code here
+function interrupter (interruptingWord) {
+  return function (word) {
+    let str = word.split(' ') 
+    return str.join( ' ' + interruptingWord + ' ' )
+  }
+}
+
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
+
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
